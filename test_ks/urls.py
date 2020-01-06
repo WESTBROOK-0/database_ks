5e1bf03d321ee3f17aae406f092e9f9cc46d27c7
WESTBROOK-0 <59240114+WESTBROOK-0@users.noreply.github.com>
2020-01-06 10:40:33 +0800

    刚刚更换

--- test_ks/urls.py
+++ test_ks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, re_path
 from .  import views
 urlpatterns = [
-    path('index/',views.index),
-    re_path('(\d+)/',views.detail)
+    path('signup/',views.signup, name='signup'),
+    path('index/',views.index,name='index'),
+    re_path('(\d+)/',views.detail,name='detail')
 
 ]
--- test_ks/views.py
+++ test_ks/views.py
@@ -6,13 +6,19 @@ from test_ks.models import Chubanshe
 from test_ks.models import Jiaocai
 from test_ks.models import Dinggou
 from test_ks.models import Xuexiao
+def signup(request):
+    list = Chubanshe.objects.all()
+    context = {'cbslist': list}
+    if request.method =='POST':
+        return render(request, 'test_ks/index.html',context)
+    else:
+        return render(request,'test_ks/signup.html')
+
 def index(request):
-    #return HttpResponse('hello world')
-    #context={'title':'django首页','list':range(10)}
-    #return render(request,'test_ks/index.html',context)
     list=Chubanshe.objects.all()
     context={'cbslist':list}
     return render(request,'test_ks/index.html',context)
+
 def detail(request,id):
     list=Chubanshe.objects.get(cbs_id=id).jiaocai_set.all()
     list2 = Chubanshe.objects.get(cbs_id=id).dinggou_set.all()
